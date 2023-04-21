ccfadb7e42b2557952a93bc2e385d2646786b1fb
Deltaqw <etude445@gmail.com>
2023-04-21 23:33:27 +0300

    zadanie 2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/mainScreen/main-screen';
+
+ type AppScreenProps = {
+	errorsCount: number;
+
+};
+
+function App( {errorsCount} : AppScreenProps ): JSX.Element {
+  return (<MainScreen errorsCount={errorsCount}/>);
 }
+
 
 export default App;
