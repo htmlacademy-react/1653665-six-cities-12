3ec70cf1177416490a2f0b43861cc6647d643891
Deltaqw <etude445@gmail.com>
2023-04-23 22:26:21 +0300

    zadanie 3-1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { Counter, City } from './utils/consts';
+import { renderArrayMocks } from './mocks/render';
 
-const Setting = {
-  ErrorsCount: 777,
+const Settings = {
+  countOffers: Counter.Count_offers,
+  isAuth: false,
+  cityCatalog: City,
 } as const;
+
+const OfferDataSet = {
+  offers: renderArrayMocks(Counter.Offers),
+  favoritesOffers: renderArrayMocks(Counter.Favorites_offers)
+} as const;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +22,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App errorsCount={Setting.ErrorsCount}/>
+    <App countOffers={Settings.countOffers} isAuth={Settings.isAuth} offers={OfferDataSet.offers}
+      cityCatalog={City}
+    />
   </React.StrictMode>,
 );
